a003cea4491b1104d5fb72588148834017453761
tkitamoto <ta9ya86@takuyakt.local>
2016-11-29 01:04:55 +0900

    pretty=formatを利用

--- verify_commit.sh
+++ verify_commit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-target_server="https://github.com/github/backup-utils.git"
-target_repository=backup-utils
+target_server="https://github.com/takuyakt/poc_script.git"
+target_repository=.
 target_branch=master
-last_git_revision=8987607
+last_git_revision=c1c323c
 git_revision=HEAD
-IFS=$'\n';
+#IFS=$'\n';
 
 if [ ! -e ${target_repository} ]; then
  #git clone
@@ -17,28 +17,31 @@ git pull
 #ターゲットブランチを見つける。
 git checkout -fb ${target_branch}
 
+#commit hash値一覧を取得する。
+
+commit_hashs=`git log ${target_branch} --pretty=format:"%H" ${last_git_revision}..${git_revision}` 
+
+
 #ここからloop処理をする。
-while [ "${last_git_revision}" != ${git_revision} ]
+for hash in ${commit_hashs}
 do
  declare -i cnt=0
  # --no-mergesは外す。
- git_files_log=`git log ${target_branch} --name-only --oneline ${git_revision}^..${git_revision}` 
- for line in  ${git_files_log}
+ git_files=`git show ${target_branch} ${hash} --pretty="format:" --name-only` 
+ for line in  ${git_files}
  do
-   if [ ${cnt} -eq 0 ]; then
+  # if [ ${cnt} -eq 0 ]; then
     # hash値とbranch名が表示される。
-    hash_and_branch=${line}
-    cnt=`expr ${cnt} + 1`
-   else
+  #  hash_and_branch=${line}
+  #  cnt=`expr ${cnt} + 1`
+  # else
   #file sizeを確認する。
    file_size=`ls -la ${line} | awk '{ print $5 }'`
   #MIME-typeを確認する。
    mime_type=`file -i ${line}`
-   output="${output}¥n${hash_and_branch},${file_size},${mime_type}"
-  fi
+   output="${output}¥n${hash},${file_size},${mime_type}"
+ # fi
   done
- #git revisionを更新
- git_revision=`echo ${hash_and_branch} | awk '{print $1}'`
 done
 echo -e ${output}
 
